5c6e8dcd922554bada2a2e2867a66e848b97a903
Nitesh Agrawal <nitesh.agrawal@habilelabs.io>
2023-12-12 10:33:41 +0530

    remove predefined template and add header footer

---
+++ src/app/components/shared/header/header.component.ts
@@ -0,0 +1,18 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import {MatButtonModule} from '@angular/material/button'
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [MatButtonModule],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.less',
+})
+export class HeaderComponent {
+  constructor(private router: Router) {}
+
+  goTo = (url: any) => {
+    this.router.navigate(url);
+  };
+}
