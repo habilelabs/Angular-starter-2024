ba6c7576994fd6f9eae3c1e23a4eecf0f87ee2d2
Nitesh Agrawal <nitesh.agrawal@habilelabs.io>
2023-12-29 01:39:02 +0530

    add guard and routing

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,23 @@
 import { Routes } from '@angular/router';
+import { authGuard } from './guards/auth.guard';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path:'login',
+        loadChildren: () => import('./components/login/login.module').then((item)=>item.LoginModule),
+    },
+    {
+        path:'home',
+        loadComponent: () => import('./components/home/home.component').then((item)=>item.HomeComponent),
+    },
+    {
+        path:'about',
+        loadChildren: () => import('./components/protected/protected.module').then(item=>item.ProtectedModule),
+        canActivate: [authGuard]
+    },
+    {
+        path:'',
+        redirectTo: 'home',
+        pathMatch:'full'
+    }
+];
